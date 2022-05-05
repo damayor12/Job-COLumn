434da98c037f8bcbf1f9c98c1cc7b0a9cd39d653
Busayo <damayor12@yahoo.com>
2022-05-05 11:54:58 +0200

    cypress tests added

--- client/cypress/integration/init.spec.js
+++ client/cypress/integration/init.spec.js
@@ -1,25 +1,32 @@
-/* eslint-disable no-undef */
 describe('Cypress', () => {
-  it('is working', () => {
+  it('testing', () => {
     // eslint-disable-next-line jest/valid-expect
     expect(true).to.equal(true);
   });
 
-  it('visits the app', () => {
+  it('visits the home page', () => {
     cy.visit('http://localhost:3000');
   });
 
-  it('should type into the input', () => {
-    cy.get('.numeric-test').type(30000)
-  });
+  // it('should have default dark mode', () => {
+  //   cy.get('.welcome').should('have.class', 'dark');
+  // });
 
-
-  it('backspace', () => {
+  it('should empty the input', () => {
     cy.get('.numeric-test').type('{selectall}{backspace}');
   });
 
+  it('should type into the input', () => {
+    cy.get('.numeric-test').type(30000);
+  });
 
+  it('should select the darkmode button', () => {
+    cy.get('[role="darkmode"]').last().click();
+  });
 
+  it('should navigate to the jobs page', () => {
+    cy.get('[role="primary"]').last().click();
 
-  
+    cy.location('pathname').should('eq', '/jobs');
+  });
 });
